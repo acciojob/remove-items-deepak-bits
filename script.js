b0ef6dc6ff16c6fcb7053493c66b442853e072e6
Deepak Kumar <40654292+deepak-bits@users.noreply.github.com>
2023-07-15 20:00:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 //your JS code here. If required.
+const selectEl = document.getElementById('colorSelect');
+const btn = document.querySelector('form > input');
+// console.log(selectEl, form);
+
+btn.addEventListener('click', (e) => {
+    // console.log('abc');
+    // e.preventDefault();
+    const options = selectEl.children;
+    console.log(options);
+
+    const filteredOptions = [];
+
+    for(let option of options) {
+        // console.log(option);
+        if(!option.selected) filteredOptions.push(option);    
+    }
+
+    // options.filter(option => !option.selected);
+    // console.log(filteredOptions);
+    selectEl.innerHTML = filteredOptions.map(option => `<option>${option.outerText}</option>`).join();
+})
